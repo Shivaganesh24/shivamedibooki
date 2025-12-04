5082df8f638846c48edfa158c204861ffb0719fd
SHASHI <shivaaliasshashi@gmail.com>
2025-12-04 16:08:03 +0000

    still i cannot see images

--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -7,4 +7,5 @@ export type ImagePlaceholder = {
   imageHint: string;
 };
 
+// The JSON file is an object with a 'placeholderImages' key which is the array.
 export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;
